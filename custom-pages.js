5e09eb0407db302057bee30f7fb8341b5ddf950f
VandeurenGlenn <vandeurenglenn@gmail.com>
2017-08-31 22:14:50 +0200

    :trollface: Version: 0.9.0-preview.2

---
+++ custom-pages.js
@@ -0,0 +1,77 @@
+import LitMixin from './../backed/mixins/lit-mixin.js';
+import CustomSelectMixin from './../custom-select-mixins/custom-select-mixin.js';
+
+/**
+ * @extends HTMLElement
+ */
+class CustomPages extends LitMixin(CustomSelectMixin(HTMLElement)) {
+  constructor() {
+    super();
+  }
+  isEvenNumber(number) {
+    return Boolean(number % 2 === 0)
+  }
+
+  /**
+   * set animation class when slot changes
+   */
+  slotchange() {
+    super.slotchange();
+    let call = 0;
+    for (const child of this.slotted.assignedNodes()) {
+      if (child && child.nodeType === 1) {
+        child.style.zIndex = 99 - call;
+        if (this.isEvenNumber(call++)) {
+          child.classList.add('animate-down');
+        } else {
+          child.classList.add('animate-up');
+        }
+      }
+    }
+  }
+
+  render() {
+    return html`
+      <style>
+        :host {
+          flex: 1;
+          position: relative;
+          --primary-background-color: #ECEFF1;
+          overflow: hidden;
+        }
+        ::slotted(*) {
+          display: flex;
+          position: absolute;
+          opacity: 0;
+          pointer-events: none;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          transition: transform ease-out 160ms, opacity ease-out 60ms;
+          /*transform: scale(0.5);*/
+          transform-origin: left;
+        }
+        ::slotted(.animate-up) {
+          transform: translateY(-120%);
+        }
+        ::slotted(.animate-down) {
+          transform: translateY(120%);
+        }
+        ::slotted(.custom-selected) {
+          opacity: 1;
+          pointer-events: auto;
+          transform: translateY(0);
+          transition: transform ease-in 160ms, opacity ease-in 320ms;
+          max-height: 100%;
+          max-width: 100%;
+        }
+      </style>
+      <!-- TODO: scale animation, ace doesn't resize that well ... -->
+      <div class="wrapper">
+        <slot></slot>
+      </div>
+    `;
+  }
+};
+export default customElements.define('custom-pages', CustomPages);
